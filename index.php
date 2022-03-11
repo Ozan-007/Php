586edfd1b49458ea86c1595c7781bf6cf5516bb3
Ozan <ozan28061@gmail.com>
2022-03-11 14:24:09 +0100

    php initialized

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignments</title>
+    <link rel="shortcut icon" href="../img/icons8-assignment-64.png" type="image/x-icon">
+</head>
+<body>
+    First Element
+</body>
+</html>
