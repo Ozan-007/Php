48ffc3a06e82b695efd3f4cbc5d3c4059c59672b
Ozan <ozan28061@gmail.com>
2022-03-12 19:10:53 +0100

    2 questions are finished

--- index.php
+++ index.php
@@ -8,6 +8,61 @@
     <link rel="shortcut icon" href="../img/icons8-assignment-64.png" type="image/x-icon">
 </head>
 <body>
-    First Element
+<!-- 1.Write a PHP program to compute the sum of the two given integer values. If the two values are the same, then returns triple their sum. -->
+
+<?php 
+    
+    // $a = 200;
+    // $b = 200;
+
+    // if ($a == $b) {
+    //     echo($a*3);
+    //   }
+    // else {
+    //     echo($a+$b);
+    // }
+    
+    ?> 
+
+     
+     <!-- 2.Write a PHP program to check two given integers, and return true if one of them is 30 or if their sum is 30. -->
+     
+     
+    <?php 
+    
+    // $a = 0;
+    // $b = 20;
+
+    // if ($a+$b == 30) {
+    //     echo('true');
+    //   }
+    // else if ($a == 30){
+    //     echo('true');
+    // }
+    // else if ($b == 30){
+    //     echo('true');
+    // }
+    // else{
+    //     echo('false');
+    // }
+    
+        ?> 
+
+
+<!-- 3.Write a PHP program to check a given integer and return true if it's even. -->
+
+
+
+
+
+<!-- 4.Write a PHP program to create a new string where 'if' is added to the front of a given string. Ifthe string already begins with 'if', return the string unchanged. -->
+<!-- 5.Write a PHP program to remove the character in a given position of a given string. The given position will be in the range 0..string length -1 inclusive. -->
+<!-- 6.Write a PHP program to exchange the first and last characters in a given string and return the new string. -->
+<!-- 7.Write a PHP program to check if a given positive number is a multiple of 3 or a multiple of 7. -->
+<!-- 8.Write a PHP program to check the largest number among three given integers. -->
+<!-- 9.Write a PHP program to check which number nearest to the value 100 among two given integers. Return 0 if the two numbers are equal. -->
+<!-- 10.Write a PHP program to compute the sum of two given non-negative integers x and y as long as the sum has the same number of digits as x. If the sum has more digits than x then return x without y. -->
+<!-- 11.Write a PHP program to compute the sum of the digits of a number. -->
+
 </body>
 </html>
