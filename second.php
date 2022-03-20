22c69d6c780e8dcbebb26a58ac272a627ecc3ac2
Ozan <ozan28061@gmail.com>
2022-03-20 12:52:02 +0100

    seondweek's assignments are added.

---
+++ second.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignments-2</title>
+    <link rel="shortcut icon" href="../img/icons8-assignment-64.png" type="image/x-icon">
+</head>
+<body>
+<!-- 1.Create a script to construct the following pattern, using nested for loop:
+
+    *  
+    * *  
+    * * *  
+    * * * *  
+    * * * * *
+
+    * * * * *
+    * * * *
+    * * *
+    **
+    *
+    
+    * * * * *
+     * * * *
+      * * * 
+       * * 
+        *
+    
+        *  
+       * * 
+      * * * 
+     * * * *
+    * * * * *  
+
+
+    2.Write a program which will give you all of the combinations of 3 digits, printed in a comma delimited format.
+
+
+    3.Write a PHP script that creates the following table (use for loops):1234567891024681012141618203691215182124273048121620242832364051015202530354045506121824303642485460714212835424956637081624324048566472809182736455463728190102030405060708090100
+
+    4.Write a program which will print all primary numbers in the range of 1 to 100.
+    
+    5.Write a program that will execute the bubble sort algorithm for the given one-dimensional array.
+    
+    6.Write a PHP script to calculate and display average temperature, five lowest and highest temperatures.Recorded temperatures : 78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73
+    
+    7.Write a program which will remove element on given position from given one dimensional array 
+    (remember about indexing items).
+    
+    8.Write a program which will create one dimensional array from two given arrays (don’t use predefined function, only for loops).
+    
+    9.Write a program that will implement the matrix addition algorithm (remember about validation).
+    
+    10.Write a program that will implement the matrix multiplicationalgorithm (remember about validation).     -->
+
+
+
+
+</body>
+</html>
