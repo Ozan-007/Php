cdf20eb92f2866c4d796ed1b607c6512ed7bac08
Ozan <ozan28061@gmail.com>
2022-03-28 16:07:24 +0200

    Assignments are added.

---
+++ rest/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rest Assignments</title>
+</head>
+<body>
+
+<!-- Assignment-7 -->
+
+<!-- <?php
+    $a = 0;
+    while($a <= 100){
+            if($a%10 ==  0){
+                echo "-$a";
+            }
+            $a++;
+        }           
+        ?> -->
+
+<!-- Assignment-8 -->
+
+
+<?php
+    // $x = 2000;
+    // do {
+    //     echo"$x <br>";
+    //     $x+=4;
+    // } while ($x <= 2100);
+?> 
+
+<?php
+    // $x = 2000;
+    // do {
+    //     echo"$x <br>";
+    //     $x+=4;
+    // } while ($x <= 2100);
+?> 
+
+<?php
+//     $colors = array("red","purple","blue");
+//     foreach ($colors as $value) {
+//         echo "$value <br>";
+//     }
+
+//     //!! Difference here is one of the array we defined has key-value pairs. we took keys and then we assigned every value to $values variable. In the other one we directly took the values from the array.!!
+
+//     $ages = array("James" => 20,"Ozan" => 28,"Can" => 30);
+//     foreach ($ages as $names => $values) {
+//         echo  "$values<br>";
+//     }
+ ?>
+
+
+
+
+</body>
+</html>
